3011aefbb6d84fd87b802b369a61a4a7ab567b49
Duru C. Mikael <durumikaelc@gmail.com>
2022-11-16 17:09:27 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
+import { StrictMode } from "react";
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useState } from "react";
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  const [count, setCount] = useState(0);
+
+  const Increase = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <main className="h-screen grid place-content-center font-poppins">
+      <section className="container mx-auto text-center">
+        <h1 className="text-7xl">Counter</h1>
+        <div className="text-7xl py-9">{count}</div>
+        <div className='flex justify-center gap-5'>
+          <button className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded-full text-xl" onClick={() => setCount(count - 1)}>Decrease</button>
+          <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full text-xl" onClick={Increase}>Increase</button>
+        </div>
+        <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full text-xl my-7" onClick={() => setCount(0)}>
+            Reset
+        </button>
+      </section>
+    </main>
+  );
+}
+
+
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/index.js",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'] 
+      },
+    },
+  },
+  plugins: [],
+}
